5b3cda397f2b8d701f7cd232dcd30532d1540f33
ErinLee <erin.lee22@education.nsw.gov.au>
2024-08-21 22:40:23 +1000

    wrote README and on coding for showCharts

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ quit = False
 original_df = pd.read_csv('Deaths_in_Australia.csv')
 
 
-deaths_df = pd.read_csv('Deaths_in_Australia.csv')
+deaths_df = pd.read_csv('Deaths_in_Australia.csv'
+                             header=None
+                             names=['Australia', 'Date', 'COD'])
                             
 
 def showOriginalData():
@@ -18,11 +20,11 @@ def showUpdatedData():
 def showCharts():
     deaths_df.plot(
                     kind='bar',
-                    x='Country',
-                    y='Date',
+                    x='Date',
+                    y='COD',
                     color='blue',
                     alpha=0.3,
-                    title='Annual Deaths in Australia')
+                    title='Annual Deaths and Causes in Australia')
     plt.show()
 
 def userOptions():
@@ -33,7 +35,7 @@ def userOptions():
           Please select an option:
           1 - Show the original dataset
           2 - Show the updated data frame
-          3 - Show the graph for the amount of death per year
+          3 - Show the graph for the amount of death with causes per year in Australia
           4 - Quit program
         """)
     
