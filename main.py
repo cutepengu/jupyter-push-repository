0fc675745d701b16c50138c9cf6f68115e2b59d8
cutepengu <erin.lee22@education.nsw.gov.au>
2024-08-23 15:00:44 +1000

    dontm
    done

--- main.py
+++ main.py
@@ -5,19 +5,10 @@ quit = False
 
 original_df = pd.read_csv('Deaths_in_Australia.csv')
 
-deaths_df = original_df.Show(rows=['Australia'])
+
+deaths_df = original_df.drop(columns=['Code'])
 deaths_df.head()
-
-<<<<<<< HEAD
-#deaths_df = pd.read_csv('Deaths_in_Australia.csv'
-  #                      )
-=======
-deaths_df = pd.read_csv('Deaths_in_Australia.csv'
-                             header=None
-                             names=['Australia', 'Date', 'COD'])
->>>>>>> 5b3cda397f2b8d701f7cd232dcd30532d1540f33
-                            
-
+                    
 def showOriginalData():
     print(original_df)
 
@@ -25,10 +16,11 @@ def showUpdatedData():
     print(deaths_df)
 
 def showCharts():
+    COD = input("Enter cause of death: ")
     deaths_df.plot(
                     kind='bar',
-                    x='Date',
-                    y='COD',
+                    x='Year',
+                    y=COD,
                     color='blue',
                     alpha=0.3,
                     title='Annual Deaths and Causes in Australia')
@@ -46,22 +38,22 @@ def userOptions():
           4 - Quit program
         """)
     
-    try:
-        choice = int(input('Enter Selection: '))
 
-        if choice == 1:
-            showOriginalData()
-        elif choice == 2:
-            showUpdatedData()
-        elif choice == 3:
-            showCharts()
-        elif choice == 4:
-            quit = True
-        else:
-            print('A number between 1 and 4, come on!')
+    choice = int(input('Enter Selection: '))
 
-    except:
-        print('Enter a number, it is not that hard.')
+    if choice == 1:
+        showOriginalData()
+    elif choice == 2:
+        showUpdatedData()
+    elif choice == 3:
+        showCharts()
+    elif choice == 4:
+        quit = True
+    else:
+        print('A number between 1 and 4, come on!')
+
+
+        #print('Enter a number, it is not that hard.')
 
    
 while not quit:
